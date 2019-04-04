62d3b2a09a498541af62942cb158bb6246ae2bec
Rita Cerdeira <ritacerdeira@ua.pt>
2019-04-04 16:27:17 +0100

    First commit

---
+++ src/main/java/com/projetotqs/prettycloud/App.java
@@ -0,0 +1,13 @@
+package com.projetotqs.prettycloud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
